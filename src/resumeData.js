3279dc4d42fe6e643a7de8c92695ea91e608b1e1
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-14 21:06:22 -0400

    second push

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/jamilohayia",
+    "name": "Jamil Ohayia",
+    "role": "Consultant",
+    "linkedinId":"https://www.linkedin.com/in/jamilohayia/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am junior developer interested in collaborating with people.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jamilohayia/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jamilohayia",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"",
+    "address":"NYC",
+    "website":"https://github.com/jamilohayia",
+    "education":[
+      {
+        "UniversityName":"Columbia University",
+        "specialization":"Political Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":""
+      },
+      {
+        // "UniversityName":"Some University",
+        // "specialization":"Some specialization",
+        // "MonthOfPassing":"Jan",
+        // "YearOfPassing":"2018",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"ACA Complaince Group",
+        "specialization":"Financial Services",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2017",
+        "Achievements":""
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
